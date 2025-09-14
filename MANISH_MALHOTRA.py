233d06f5aaca1427abc0f81b3305c867fe04d3c2
CoderZiyom-1 <khanmoyiz@gmail.com>
2025-09-14 16:57:45 +0530

    Update MANISH_MALHOTRA.py

--- MANISH_MALHOTRA.py
+++ MANISH_MALHOTRA.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from create_mem_2 import analyze_colors, get_clip_embedding, run_query
 
-st.set_page_config(page_title="Fashion bot", page_icon="👗", layout="centered")
+st.set_page_config(page_title="Fashion bot", page_icon="W", layout="centered")
 st.title(" Fashion Police")
 
-uploaded = st.file_uploader("📸 Upload an image of your outfit", type=["jpg", "png"])
+uploaded = st.file_uploader("Upload an image of your outfit", type=["jpg", "png"])
 
 outfit_info = ""  # to store extracted details
 
@@ -14,7 +14,7 @@ if uploaded:
 
     st.image("temp.jpg", caption="Your Uploaded Outfit", use_container_width=True)
 
-    st.subheader("🎨 Outfit Color Analysis")
+    st.subheader("Outfit Color Analysis")
     color_feedback, named_colors = analyze_colors("temp.jpg")
     outfit_info = f"Detected outfit colors: {', '.join(named_colors)}"
     st.markdown(color_feedback)
@@ -27,7 +27,8 @@ user_query = st.text_input("Ask about anything fashion related( or upload your o
 
 if user_query:
     response = run_query(user_query, outfit_info=outfit_info)
-    st.subheader("🧾 Fashion Q&A Result")
+    st.subheader("Fashion Q&A Result")
     st.markdown(response)
+
 
 
